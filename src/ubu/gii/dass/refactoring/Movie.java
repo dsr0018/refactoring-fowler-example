156a798712a9a5cd4f1e988b372bb89cf2ecb04f
danielsetoubu <dsr0018@alu.ubu.es>
2020-05-07 19:00:47 +0200

    Refactor Move Method Movie.getCharge(...) to Price.getCharge(...)
    Move method 'ubu.gii.dass.refactoring.Movie.getCharge(...)' to
    'ubu.gii.dass.refactoring.Movie._price'
    - Original project: 'refactoring-fowler-example'
    - Original element: 'ubu.gii.dass.refactoring.Movie.getCharge(...)'
    - Keep original element as delegate to refactored element
    - Moved method: N/A
    - Target element: 'ubu.gii.dass.refactoring.Movie._price'
    - Method name: 'getCharge'

--- src/ubu/gii/dass/refactoring/Movie.java
+++ src/ubu/gii/dass/refactoring/Movie.java
@@ -49,24 +49,7 @@ public class Movie {
 	}
 
 	public double getCharge(Rental rental) {
-		double result=0;
-		// determine amounts for each line
-		switch (rental.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if (rental.getDaysRented() > 2)
-				result += (rental.getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			result += rental.getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			result += 1.5;
-			if (rental.getDaysRented() > 3)
-				result += (rental.getDaysRented() - 3) * 1.5;
-			break;
-		}
-		return result;
+		return _price.getCharge(rental);
 	}
 
 	public int getFrecuentRenterPoints(Rental rental) {
